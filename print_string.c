a05e2e4face52aff6a6b7780932a97a3af8308b6
Karl <tekamkarl@gmail.com>
2023-08-20 14:58:51 +0200

    print a string

---
+++ print_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_string - print a string on the output
+ * @str: string to print
+ * 
+ * Return: the lenght of the string printed
+*/
+int print_string(char *str)
+{
+    int len = 0;
+    while (str[len])
+    {
+        len++;
+    }
+    write(1, str, len);
+    return (len);
+}

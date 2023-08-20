6bff953cbf6c8b5c66253281f7bcc9544a02b5c7
Karl <tekamkarl@gmail.com>
2023-08-20 14:58:00 +0200

    header file

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <stdarg.h> /* for variadic macros */
+#include <unistd.h> /* for write function */
+
+int _printf(const char *format, ...);
+int test(const char *format, int index, va_list args);
+int print_string(char *str);
+int print_percent(void);
+int print_char(char ch);
+
+#endif

626c31ffd2ef251a3aa39ad0b95e0ba61386b93b
Karl <tekamkarl@gmail.com>
2023-08-20 14:57:49 +0200

    produces output according to a format

---
+++ _printf.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _printf -produces output according to a format
+ * @format: format that can influence output
+ *
+ * Return:  the number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+    va_list args;
+    int i, printed_char;
+    va_start(args, format);
+    
+    i = 0;
+    while (format[i])
+    {
+        if (format[i] != '%')
+        {
+            printed_char += print_char(format[i]);
+        }
+        else
+        {
+            i++;
+            printed_char += test(format, i, args);
+        }
+        i++;
+    }
+
+    va_end(args);
+
+    return (printed_char);
+}
+
+
+
+
+

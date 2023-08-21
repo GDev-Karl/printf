4a970ae008c494d96cfc358ed55ea54233e8940b
Karl <tekamkarl@gmail.com>
2023-08-21 03:31:19 +0200

    format character, strings, and percentage

--- test.c
+++ test.c
@@ -1,32 +1,31 @@
 #include "main.h"
 
 /**
- * test - format character, stirngs and percentage
+ * test - format character, strings, and percentage
  * @format: format that can influence output
  * @index: the index of the character or a format symbol
  * @args: arguments list
- * 
+ *
  * Return: printed characters
-*/
+ */
 int test(const char *format, int index, va_list args)
 {
-    char character = format[index];
-    int printed_chars = 0;
+	char character = format[index];
+	int printed_chars = 0;
 
-    switch (character)
-    {
-    case 'c':
-        printed_chars += print_char((char)va_arg(args, int));
-        break;
-    case 's':
-        printed_chars += print_string(va_arg(args, char *));
-        break;
-    case '%':
-        printed_chars += print_percent();
-        break;
-    default:
-        break;
-    }
-
-    return printed_chars;
+	switch (character)
+	{
+		case 'c':
+			printed_chars += print_char((char)va_arg(args, int));
+			break;
+		case 's':
+			printed_chars += print_string(va_arg(args, char *));
+			break;
+		case '%':
+			printed_chars += print_percent();
+			break;
+		default:
+			break;
+	}
+	return (printed_chars);
 }

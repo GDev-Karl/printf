432492671d09db05f8addd8dee9320961ecb7d53
Karl <tekamkarl@gmail.com>
2023-08-21 04:23:43 +0200

    header file

--- main.h
+++ main.h
@@ -5,7 +5,7 @@
 #include <unistd.h> /* for write function */
 
 int _printf(const char *format, ...);
-int test(const char *format, int index, va_list args);
+int test(const char *format, va_list args);
 int print_string(char *str);
 int print_percent(void);
 int print_char(char ch);

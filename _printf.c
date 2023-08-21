3a0ca4c9cd73a5010dbbfc07a93416a8e7f0ab14
Karl <tekamkarl@gmail.com>
2023-08-21 03:28:43 +0200

    produces output according to a format

--- _printf.c
+++ _printf.c
@@ -9,31 +9,26 @@
 
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int i, printed_char;
-    va_start(args, format);
-    
-    i = 0;
-    while (format[i])
-    {
-        if (format[i] != '%')
-        {
-            printed_char += print_char(format[i]);
-        }
-        else
-        {
-            i++;
-            printed_char += test(format, i, args);
-        }
-        i++;
-    }
+	va_list args;
+	int i, printed_char = 0;
 
-    va_end(args);
+	va_start(args, format);
+	i = 0;
 
-    return (printed_char);
+	while (format[i])
+	{
+		if (format[i] != '%')
+		{
+			printed_char += print_char(format[i]);
+		}
+		else
+		{
+			i++;
+			printed_char += test(format, i, args);
+		}
+		i++;
+	}
+	va_end(args);
+
+	return (printed_char);
 }
-
-
-
-
-

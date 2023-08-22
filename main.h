f64fb7d49f9e9ca8f5798f288727d5ae465921fd
Abrshhh <abreham.dessiem@gmail.com>
2023-08-22 07:39:47 -0700

    updated main.h

--- main.h
+++ main.h
@@ -10,7 +10,6 @@ int test(const char *format, va_list args);
 int print_string(char *str);
 int print_percent(void);
 int print_char(char ch);
-int _printf_decimal(const char *format, ...);
 int print_int(int num);
 
 #endif
